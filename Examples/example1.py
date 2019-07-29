abf7a929c0e9ac805985e6d1d95f71f7674241ab
Ending2015a <zexlus1126@gapp.nthu.edu.tw>
2019-07-29 13:39:13 +0800

    update example

--- Examples/example1.py
+++ Examples/example1.py
@@ -3,7 +3,6 @@ import sys
 import time
 import logging
 
-
 from nao_search import epd
 
 from nao_search.common.logger import LoggingConfig
@@ -13,16 +12,14 @@ from nao_search.common.utils import min_max_normalization
 from nao_search.common.utils import get_top_n
 
 
-LoggingConfig.Use(filename='nao_seq_search.log', output_to_file=True, level='DEBUG')
-
-
-# === generate sequences ===
+# === Generate sequences ===
 
 seqs = random_sequences(length=60,
                         seq_num=300,
                         vocab_size=20)
 
-# === calculate score ===
+
+# === Calculate scores ===
 
 def get_score(seq):
     return sum(seq)
@@ -33,19 +30,29 @@ for seq in seqs:
 
 norm_scores = min_max_normalization(scores)
 
-# create model
+# === Create model ===
+
 epd_model = epd.BaseModel(source_length=60,
-                          encoder_vocab_size=20,
-                          decoder_vocab_size=20,
+                          encoder_vocab_size=21,        # vocab_size + <SOS> = 21
+                          decoder_vocab_size=21,        # vocab_size + <SOS> = 21
+                          batch_size=50,
                           num_cpu=1,
                           tensorboard_log='nao_logs',
-                          full_tensorboard_log=False)
+                          input_processing=True,
+                          full_tensorboard_log=False
+                          )
+
+# === Learn ===
 
 epd_model.learn(X=seqs,
                 y=norm_scores,
-                log_interval=1,
-                tb_log_name='seq_search')
+                epochs=1000,
+                eval_interval=50,     # epochs
+                log_interval=1,       # epochs
+                tb_log_name='seq_search'
+                )
 
+
+# === Save model ===
 
 epd_model.save('nao_seq_search.model')
-
